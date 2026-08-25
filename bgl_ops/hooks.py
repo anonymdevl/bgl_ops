1c00ec67e134990ee68d9b27a10777dd9b6848d4
Michael <kubiappiahmichael@gmail.com>
2026-08-25 13:33:02 -0400

    v0.2.0: full 5-tab command center, bulk log sheet, BGL Vehicle master, v16 workspace fix

--- bgl_ops/hooks.py
+++ bgl_ops/hooks.py
@@ -1,8 +1,8 @@
 app_name = "bgl_ops"
 app_title = "BGL Ops"
-app_publisher = "Michael"
+app_publisher = "Powersoft"
 app_description = "Trips & cubic tracking, staff loans, leave visibility and an executive command center for Betonsa Ghana Limited."
-app_email = "michael@powersoftsystem.com"
+app_email = "ai4powersoft@gmail.com"
 app_license = "Proprietary"
 
 # Target stack: Frappe v16 / ERPNext v16 / Frappe HR v16 (as on bgl.powersoftsystem.com)
@@ -19,6 +19,7 @@ fixtures = [
     {"dt": "Custom Field", "filters": [["name", "in", [
         "Daily Trip Log-salary_component",
         "Employee-custom_no_of_days_suspended", "Employee-ssnit_number",
+        "Employee-custom_truck_no",
         "Employee-custom_take_home_section", "Employee-custom_target_take_home",
         "Employee-custom_basic_percentage", "Employee-custom_thp_cb",
         "Employee-custom_standard_working_days", "Employee-custom_thp_split_sb",
@@ -69,4 +70,6 @@ fixtures = [
     ]]]},
 ]
 
-website_route_rules = []
+website_route_rules = [
+    {"from_route": "/command-center", "to_route": "command_center"},
+]
